3a21ba20f332b654cd70b2ce4fc6147127232407
Riccardo Accomando <riccardoaccoma@gmail.com>
2022-07-21 15:18:11 +0200

    changes

--- nextjs/src/components/game/index.tsx
+++ nextjs/src/components/game/index.tsx
@@ -1,31 +1,34 @@
 import { FC } from 'react';
-import { Circle, Line, Layer, Stage, Rect, Text, Image } from 'react-konva';
+import { Circle, Layer, Stage, Rect, Image } from 'react-konva';
 import useImage from 'use-image';
 
-const Component: FC = () => {
+const Component: FC<{ posY: number, oPosY: number }> = ({ posY, oPosY }) => {
 
   const [me] = useImage('https://images.all-free-download.com/images/graphiclarge/cartoon_pattern_background_05_vector_180955.jpg');
   const [opponent] = useImage('https://static.vecteezy.com/system/resources/thumbnails/000/692/310/small/seamless-pattern-of-cute-cartoon-planets-in-space-background.jpg');
 
+
   return (
     <Stage width={1024} height={576}>
+
       <Layer>
 
-        <Rect
-          x={0}
-          y={0}
-          width={512}
-          height={576}
-          fillPatternImage={me}
-        />
-        <Rect
-          x={512}
-          y={0}
-          width={512}
-          height={576}
-          fillPatternImage={opponent}
-        />
+        <Rect x={0} y={0} width={512} height={576} fillPatternImage={me} />
+        <Rect x={512} y={0} width={512} height={576} fillPatternImage={opponent} />
+
+        {/*<Rect x={0} y={0} width={1024} height={576} stroke='white' strokeWidth={6} />*/}
+        <Rect x={509} y={0} width={3} height={576} fill='white' />
+        <Circle x={512} y={288} radius={96} stroke='white' strokeWidth={3} />
+
       </Layer>
+
+      <Layer>
+
+        <Rect x={20} y={posY} width={20} height={192} fill='white' cornerRadius={5} />
+        <Rect x={984} y={oPosY} width={20} height={192} fill='white' cornerRadius={5} />
+
+      </Layer>
+
     </Stage>
   );
 };
--- nextjs/src/pages/match/[id].tsx
+++ nextjs/src/pages/match/[id].tsx
@@ -3,6 +3,7 @@ import { NextPage } from 'next';
 import { useRouter } from 'next/router';
 import { useSession } from 'src/contexts';
 import { Heartbeat, HeartBreak } from 'phosphor-react';
+import { KeyboardEventHandler, useEffect, useState } from 'react';
 
 const PongCanvas = dynamic(() => import('src/components/game'), { ssr: false });
 
@@ -13,23 +14,43 @@ const Game: NextPage = () => {
 
   const { profile } = useSession();
 
-  const me = { profile: { username: 'raccoman' }, lives: 2 };
-  const oppenent = { profile: { username: 'ntropea' }, lives: 1 };
+  const [me, setMe] = useState({ profile: { username: 'raccoman' }, posY: 10, lives: 2 });
+  const [oppenent, setOppenent] = useState({ profile: { username: 'ntropea' }, posY: 20, lives: 4 });
+
   const lapsed_time = '08:32';
+
+  const onKeyDown = (e: KeyboardEvent) => {
+
+    if (e.key === 'ArrowUp')
+      setMe({ ...me, posY: me.posY - 3 });
+
+    if (e.key === 'ArrowDown')
+      setMe({ ...me, posY: me.posY + 3 });
+
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    };
+  }, []);
 
   return (
     <div className='h-full px-5 py-2 flex flex-col items-center justify-center'>
 
-      <div className='border border-primary-400 rounded-lg overflow-hidden'>
+      <div className='flex flex-col space-y-4'>
 
-        <div className='flex items-center justify-center py-2.5 px-10 space-x-10'>
-          <p className='text-xl font-medium'>{me.profile.username}</p>
+        <div className='flex items-center justify-center py-2.5 px-10 space-x-10 border border-primary-400 rounded-lg'>
+
+          <p className='text-xl font-medium w-[150px] text-center'>{me.profile.username}</p>
           <div className='flex items-center space-x-2'>
-            {Array.from(Array(me.lives).keys()).map(() => (
-              <Heartbeat className='text-red-600' weight='duotone' size={32} />
+            {Array.from(Array(me.lives).keys()).map((value, index) => (
+              <Heartbeat key={index} className='text-red-600' weight='duotone' size={32} />
             ))}
-            {Array.from(Array(5 - me.lives).keys()).map(() => (
-              <HeartBreak className='text-primary-400' weight='duotone' size={32} />
+            {Array.from(Array(5 - me.lives).keys()).map((value, index) => (
+              <HeartBreak key={index} className='text-primary-400' weight='duotone' size={32} />
             ))}
           </div>
 
@@ -38,17 +59,20 @@ const Game: NextPage = () => {
           </div>
 
           <div className='flex items-center space-x-2'>
-            {Array.from(Array(5 - oppenent.lives).keys()).map(() => (
-              <HeartBreak className='text-primary-400' weight='duotone' size={32} />
+            {Array.from(Array(5 - oppenent.lives).keys()).map((value, index) => (
+              <HeartBreak key={index} className='text-primary-400' weight='duotone' size={32} />
             ))}
-            {Array.from(Array(oppenent.lives).keys()).map(() => (
-              <Heartbeat className='text-red-600' weight='duotone' size={32} />
+            {Array.from(Array(oppenent.lives).keys()).map((value, index) => (
+              <Heartbeat key={index} className='text-red-600' weight='duotone' size={32} />
             ))}
           </div>
-          <p className='text-xl font-medium'>{oppenent.profile.username}</p>
+          <p className='text-xl font-medium w-[150px] text-center'>{oppenent.profile.username}</p>
+
         </div>
 
-        <PongCanvas />
+        <div className='border border-primary-400 rounded-lg overflow-hidden'>
+          <PongCanvas posY={me.posY} oPosY={oppenent.posY} />
+        </div>
 
       </div>
 
